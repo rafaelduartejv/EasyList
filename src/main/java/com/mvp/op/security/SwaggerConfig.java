ae0adc2033982925388108adc557c1d1049f4a1c
Rafael Santos <109754987+rafaelduartejv@users.noreply.github.com>
2025-06-10 18:03:46 -0300

    upt: swagger.config

--- src/main/java/com/mvp/op/security/SwaggerConfig.java
+++ src/main/java/com/mvp/op/security/SwaggerConfig.java
@@ -12,8 +12,16 @@ public class SwaggerConfig {
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("Secure Login System API")
-                        .version("1.0.0")
-                        .description("API for a secure login system with JWT authentication"));
+                        .title("EasyList API")
+                        .version("0.0.1")
+                        .description("""
+                            EasyList API provides endpoints for managing Mercado Livre listings, including:
+                            - Secure user authentication with JWT
+                            - OAuth2 integration with Mercado Livre
+                            - Cloning, editing, saving drafts, and publishing product listings
+                            - Token management and user info retrieval
+                            
+                            This API enables automation of product listings and order management workflows.
+                            """));
     }
 }
